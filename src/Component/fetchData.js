aa0dab9c8621ae8445e17a5427b2fcc7e708bfa1
mraliahmadi <mr.ali.ahmadi.021@gmail.com>
2022-08-29 15:39:05 +0430

    Update

--- src/Component/fetchData.js
+++
@@ -1,9 +0,0 @@
-import wiki from 'wikipedia'
-async function fetchData(country) {
-    const page = await wiki.page(country);
-    const data = await page.summary();
-    const info = await page.infobox()
-    const body = await page.intro()
-    return data, info, body
-}
-export default fetchData;
